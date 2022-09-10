b4e59d2bb6ef895f3d28bd215e7224772dc9568c
carla-ng <carlitashata@gmail.com>
2022-09-10 22:40:39 +0200

    Creation of project - Twitter clone.

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  
+  <div id="app">
+    <UserProfile />
+  </div>
+
+</template>
+
+<script>
+import UserProfile from './components/UserProfile'
+
+export default {
+  name: 'App',
+  components: { UserProfile }
+}
+</script>
+
+<style>
+#app {
+  color: #2c3e50;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  margin-top: 60px;
+
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+}
+</style>
---
+++ src/components/UserProfile.vue
@@ -0,0 +1,90 @@
+<template>
+
+  <div class="user-profile">
+      <div class="user-profile__user-panel">
+        <h1 class="user-profile__">@{{ user.username }}</h1>
+        <div class="user-profile__follower-count">
+          <strong>Followers: </strong> {{ followers }}
+        </div>
+      </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+  name: 'UserProfile',
+
+  data() {
+
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        username: 'carla-test',
+        firstName: 'Carla',
+        lastName: 'N',
+        email: 'test@test.com',
+        isAdmin: true
+      }
+    }
+
+  },
+
+  watch: {
+
+    followers( newFollowerCount, oldFollowerCount ) {
+      if ( oldFollowerCount < newFollowerCount) {
+        console.log(`${this.user.username} has gained a follower!`)
+      }
+    }
+
+  },
+
+  computed: {
+
+    fullName() {
+      return `${this.user.firstName} ${this.user.lastName}`
+    }
+
+  },
+
+  methods: {
+
+    followUser() {
+      this.followers++
+    }
+
+  },
+
+  mounted() {
+    this.followUser()
+  }
+
+}
+</script>
+
+<style>
+.user-profile {
+    display: grid;
+    grid-gap: 50px;
+    grid-template-columns: 1fr 3fr;
+    padding: 50px 5%;
+}
+
+.user-profile__user-panel {
+    background-color: white;
+    border-radius: 5px;
+    border: 1px solid #DFE3E8;
+    margin-bottom: auto;
+    padding: 20px;
+
+    display: flex;
+    flex-direction: column;
+}
+
+h1 {
+      margin: 0;
+    }
+
+</style>
